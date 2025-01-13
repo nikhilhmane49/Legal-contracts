2470684ed173b66a3d27a8a9a77efbdb86489145
nikhilhmane49 <manenikhil1949@gmail.com>
2025-01-13 18:59:29 +0530

    first commit

---
+++ Backend/src/app.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+
+const PORT = 8080
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    
+});
