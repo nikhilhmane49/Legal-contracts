23e602d86c48c96f934185ed219b964bd1aff1db
nikhilhmane49 <manenikhil1949@gmail.com>
2025-01-15 00:28:23 +0530

    model

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -8,14 +8,26 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 
 const app = express();
+
+// console.log(process.env.MONGODB_URL);
+
+
+if (!process.env.MONGODB_URL) {
+  console.error('MONGODB_URL is not defined in the .env file');
+  process.exit(1);
+}
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
-const PORT = 8080
+const PORT = 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    
+  console.log(`Server is running on port ${PORT}`);
 });
---
+++ Backend/src/models/User.model.ts
@@ -0,0 +1,20 @@
+
+import { model, Schema } from "mongoose";
+import mongoose from "mongoose";
+
+export interface IdUser{
+    googleId: string,
+    email: string,
+    displayname: string,
+    profileImage: string,
+}
+
+
+const userSchema: Schema = new Schema({
+    googleId: { type: String, required: true , unique: true },
+    email: { type: String, required: true , unique: true },
+    displayname: { type: String, required: true },
+    profileImage: { type: String, required: true },
+})
+
+export default model <IdUser>('User', userSchema);
